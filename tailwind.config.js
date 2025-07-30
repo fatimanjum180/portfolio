335f915d8ec71ce264fc84464ea9d102291effbf
fatimanjum180 <fatimanjum180@gmail.com>
2025-07-30 15:36:13 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1.5s ease-out',
+        'fade-up': 'fadeUp 1.5s ease-out',
+        'fade-left': 'fadeLeft 1.5s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeUp: {
+          '0%': { opacity: 0, transform: 'translateY(30px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeLeft: {
+          '0%': { opacity: 0, transform: 'translateX(30px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
